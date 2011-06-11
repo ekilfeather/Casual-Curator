de00c78d1b35a9ff9dd82f1245699f415d6d24f0
Monica Harasimiuk <monica.harasimiuk@dmc.dit.ie>
2011-06-11 16:12:14 +0100

    TEST - minor changes to the search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,4 @@
-# require 'rubygems'
+require 'rubygems'
 require 'rdf/raptor'
 
 class SearchController < ApplicationController
@@ -16,8 +16,8 @@ class SearchController < ApplicationController
     @influenced = []
     @influenced_by = []
     
-    # begin
-      
+    begin
+          
       RDF::Reader.open(rdf_url) do |reader|
         reader.each_statement do |statement| 
           if statement.predicate == 'http://rdf.freebase.com/ns/type.key.namespace'
@@ -53,15 +53,15 @@ class SearchController < ApplicationController
         format.xml { render :layout => false }
       end
 
-    # rescue
-    #       # flash[:notice] = "We're sorry but there is insufficient RDF data. Please try another artist."
-    #       # redirect_to(:controller => :search, :action => :home)
-    # 
-    #       respond_to do |format|
-    #         format.html { redirect_to('/search/home', :notice => "We're sorry but there is insufficient RDF data. Please try another artist.") }
-    #         format.xml { render :layout => false }
-    #       end
-    #     end
+    rescue
+      # flash[:notice] = "We're sorry but there is insufficient RDF data. Please try another artist."
+      # redirect_to(:controller => :search, :action => :home)
+
+      respond_to do |format|
+        format.html { redirect_to('/search/home', :notice => "We're sorry but there is insufficient RDF data. Please try another artist.") }
+        format.xml { render :layout => false }
+      end
+    end
     
   end
   
